6fa6179c984b1d4256c34ab969907f328212eb4d
Vyacheslav <vzikass@icloud.com>
2024-09-12 20:10:12 +0300

    added more comments

--- utils/generatehash.go
+++ utils/generatehash.go
@@ -3,9 +3,9 @@ package utils
 import (
 	"golang.org/x/crypto/bcrypt"
 )
-
+// Generating hash (for password, tokens)
 func GenerateHash(line string) (string, error) {
-	hash, err := bcrypt.GenerateFromPassword([]byte(line), bcrypt.DefaultCost)
+	hash, err := bcrypt.GenerateFromPassword([]byte(line), bcrypt.DefaultCost) // DefaultCost = 10
 	if err != nil {
 		return "", err
 	}
