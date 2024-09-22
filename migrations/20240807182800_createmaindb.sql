d8949382c0283b62fcd0a9a72f07963d48a80df9
Vyacheslav <vzikass@icloud.com>
2024-09-22 22:16:53 +0300

    fix: relation users does not exist

--- migrations/20240807182800_createmaindb.sql
+++ migrations/20240807182800_createmaindb.sql
@@ -5,7 +5,7 @@ create table if not exists users (
     username varchar(255) unique not null,
     email varchar(255) unique not null,
     password varchar(255) not null,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 -- +goose StatementEnd
 
