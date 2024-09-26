296642b77c577205c1afd7bffa7491f69eee06b4
Vyacheslav <vzikass@icloud.com>
2024-09-26 19:00:24 +0300

    fix: integration tests failed and other

--- tests/migration-test/testdb_migration_test.go
+++ tests/migration-test/testdb_migration_test.go
@@ -16,13 +16,14 @@ var (
 	dbURL  = "postgres://postgres:193566@localhost:5432/testdb?sslmode=disable"
 )
 
-func TestMain(m *testing.M) {
+func TestMigration(t *testing.T) {
 	var err error
 
 	testDB, err = sql.Open("postgres", dbURL)
 	if err != nil {
 		log.Fatalf("Failed to connect to test database: %v", err)
 	}
+	defer testDB.Close()
 
 	currentDir, err := os.Getwd()
 	if err != nil {
@@ -42,14 +43,9 @@ func TestMain(m *testing.M) {
 		log.Fatalf("Failed to apply migrations: %v", err)
 	}
 
-	code := m.Run()
-
 	err = goose.Down(testDB, migrationDir)
 	if err != nil {
 		log.Fatalf("Failed to rollback migrations: %v", err)
 	}
 
-	testDB.Close()
-
-	os.Exit(code)
 }
