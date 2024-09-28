aff5534ef6de51836bbd7cb8e1d9197d661452c6
Vyacheslav <vzikass@icloud.com>
2024-09-28 13:25:53 +0300

    fix: SQLSTATE 42P01 error

--- tests/helpers/transactions.go
+++ tests/helpers/transactions.go
@@ -3,19 +3,45 @@ package helpers
 import (
 	"AuthDB/cmd/app/repository"
 	"context"
+	"database/sql"
 	"log"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/pressly/goose/v3"
 )
 
 var (
-	dbURL = "postgres://postgres:193566@localhost:5433/testdb?sslmode=disable"
+	DBURL = "postgres://postgres:193566@localhost:5433/testdb?sslmode=disable"
 )
+
+func UpMigrations(t *testing.T) error{
+	testDB, err := sql.Open("postgres", DBURL)
+	if err != nil {
+		log.Fatalf("Failed to connect to test database: %v", err)
+	}
+
+	currentDir, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("Failed to get current directory: %v", err)
+	}
+	migrationDir := filepath.Join(currentDir, "../../migrations")
+
+	err = goose.Up(testDB, migrationDir)
+	if err != nil {
+		log.Fatalf("Failed to apply migrations: %v", err)
+	}
+	return nil
+}
 
 // func to clear the db
 func clearDatabase(t *testing.T, pool *pgxpool.Pool) {
+	if err := UpMigrations(t); err != nil{
+		t.Fatalf("Failed to up migrations: %v", err)
+	}
 	_, err := pool.Exec(context.Background(), "TRUNCATE users RESTART IDENTITY")
 	if err != nil {
 		t.Fatalf("Failed to clear database: %v", err)
@@ -25,7 +51,7 @@ func clearDatabase(t *testing.T, pool *pgxpool.Pool) {
 func RunWithTransactions(t *testing.T, fn func(tx pgx.Tx) error) {
 	ctx := context.Background()
 
-	pool, err := repository.InitDBConn(context.Background(), dbURL)
+	pool, err := repository.InitDBConn(context.Background(), DBURL)
 	if err != nil {
 		log.Fatalf("Error initializing Test DB connection: %v\n", err)
 	}
